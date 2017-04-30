3a2c9f507eca3bae0fab49688b590efa25a19bbd
Billie Cleek <bhcleek@gmail.com>
2017-04-29 20:14:37 -0700

    fix bootstrap.sh
    Add then to if statements where it was missing.
    
    Only check for and try installing brew when the host system is Darwin.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,14 +6,17 @@ echo 'Installing Ansible'
 # prefer pip for installing python packages over the older easy_install
 #
 if ! command -v pip > /dev/null 2>&1
+then
     sudo easy_install pip
 fi
 
 if ! command -v ansible-playbook > /dev/null 2>&1
+then
     sudo CFLAGS=-Qunused-arguments CPPFLAGS=-Qunused-arguments pip install ansible
 fi
 
-if ! command -v brew > /dev/null 2>&1
+if [ "$(uname -s)" = "Darwin" ] && ! command -v brew > /dev/null 2>&1
+then
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	brew install caskroom/cask/brew-cask
 fi
