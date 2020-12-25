02bfb405c95f3860fafc6b32ab20f6aa68bdddce
Billie Cleek <bhcleek@gmail.com>
2020-12-24 23:19:36 -0800

    correct problems with bashrc

--- roles/base/files/bashrc.d/fzf.bash
+++ roles/base/files/bashrc.d/fzf.bash
@@ -1,14 +1,25 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
+if [[  "$(uname -s)" == 'Darwin' ]];
+then
+	# Setup fzf
+	# ---------
+	if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+		export PATH="$PATH:/usr/local/opt/fzf/bin"
+	fi
+
+	# Auto-completion
+	# ---------------
+	[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+
+	# Key bindings
+	# ------------
+	source "/usr/local/opt/fzf/shell/key-bindings.bash"
+elif [[ "$(uname -s)" == 'Linux' ]];
+then
+	# Auto-completion
+	# ---------------
+	[[ $- == *i* ]] && source "/usr/share/bash-completion/completions/fzf" 2> /dev/null
+
+	# Key bindings
+	# ------------
+	source "/usr/share/doc/fzf/examples/key-bindings.bash"
 fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/usr/local/opt/fzf/shell/key-bindings.bash"
-
