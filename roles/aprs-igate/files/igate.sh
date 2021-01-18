0c71dd0dd1f8472217b563d9bd9c00f4b4fa7d84
Billie Cleek <bhcleek@gmail.com>
2021-01-17 22:52:07 -0800

    split rtl_fm and direwolf into separate services
    Split rtl_fm and direwolf into separate services and stop using a shell
    script to try to control the igate pipeline.
    
    rtl_fm now runs in the aprs service with its stdout hooked up to a named
    pipe and direwolf runs in the igate service with its stdin hookup to the
    other end of the named pipe.

--- roles/aprs-igate/files/igate.sh
+++ roles/aprs-igate/files/igate.sh
@@ -4,6 +4,8 @@ set -eu -o pipefail
 
 trap 'kill $(jobs -p)' TERM EXIT
 
-rtl_fm -f 144.390M | direwolf -c /etc/aprs/sdr-igate.conf -r 24000 -D 1 -t 0 &
+#rtl_fm -f 144.390M -g 49.6 | direwolf -c /etc/aprs/sdr-igate.conf -r 24000 -D 1 -t 0 &
+#rtl_fm -f 144.390M -| direwolf -c /etc/aprs/sdr-igate.conf -r 24000 -D 1 -t 0 &
+rtl_fm -f 144.390M -| direwolf -c /etc/aprs/sdr-igate.conf -r 24000 -D 1 -t 0 -dimf &
 
 wait
