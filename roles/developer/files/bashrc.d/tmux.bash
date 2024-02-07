83e11bb9b156695681db1703d24e61b30e6d598c
Billie Cleek <bhcleek@gmail.com>
2024-02-06 16:16:50 -0800

    improve tmux session creation
    Ensure the files sourced in bashrc are ordered. This will ensure, for
    instance, that the path is set correctly when the tmux panes are
    created. This is important when a command is being run directly in a
    pane without first starting a login or interactive shell.
    
    Separate the creation of the tmux server and the creation of the default
    tmux session.
    
    Fix the godoc pane to work with latest godoc by removing the analysis
    flag from the godoc invocation.
    
    Make all the dashboard panes remain on exit so that they can easily be
    respawned.

--- roles/developer/files/bashrc.d/tmux.bash
+++ roles/developer/files/bashrc.d/tmux.bash
@@ -1,13 +1,25 @@
 function _run_tmux {
+	local session_name=default
 	# start the default tmux session if it doesn't already exist
 	if command -v tmux &> /dev/null
 	then
 		if ! tmux has-session 2> /dev/null
 		then
 			tmux start-server
-			tmux new-session -s default -n dashboard -d
-			tmux split-window -h 'ulimit -n 4096 && godoc -http=:6060 -analysis=type -notes="BUG|TODO"'
-			tmux split-window -v
+		fi
+		if ! tmux has-session -t ${session_name} 2> /dev/null
+		then
+			# set the remain-on-exit as a pane on option on each pane instead of a
+			# window option so that any additional panes on the created on the
+			# dashboard window do not inherit the option value
+			tmux new-session -s ${session_name} -n dashboard -d
+			tmux set-option -p remain-on-exit on
+			tmux set-option -w remain-on-exit on
+			pane=$(tmux split-window -P -d -h 'ulimit -n 4096 && godoc -http=:6060 -notes="BUG|TODO"')
+			tmux set-option -w remain-on-exit off
+			tmux set-option -p -t "${pane}" remain-on-exit on
+			pane=$(tmux split-window -P -d -v)
+			tmux set-option -p -t "${pane}" remain-on-exit on
 			tmux select-pane -t 0
 			tmux select-layout main-vertical
 			if [ -d "$GOPATH/src/github.com" ]
