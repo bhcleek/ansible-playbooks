2141b973ce2d43aa363d7339036bace5964db75a
Billie Cleek <bhcleek@gmail.com>
2024-02-29 10:08:02 -0800

    construct dashboard so godoc is on upper right

--- roles/developer/files/bashrc.d/tmux.bash
+++ roles/developer/files/bashrc.d/tmux.bash
@@ -9,17 +9,17 @@ function _run_tmux {
 		fi
 		if ! tmux has-session -t ${session_name} 2> /dev/null
 		then
-			# set the remain-on-exit as a pane on option on each pane instead of a
-			# window option so that any additional panes on the created on the
-			# dashboard window do not inherit the option value
+			# set the remain-on-exit option on each pane instead of a window option
+			# so that any additional panes created on the dashboard window do
+			# not inherit the option value
 			tmux new-session -s ${session_name} -n dashboard -d
 			tmux set-option -p remain-on-exit on
-			tmux set-option -w remain-on-exit on
-			pane=$(tmux split-window -P -d -h 'ulimit -n 4096 && godoc -http=:6060 -notes="BUG|TODO"')
-			tmux set-option -w remain-on-exit off
 			tmux set-option -p -t "${pane}" remain-on-exit on
 			pane=$(tmux split-window -P -d -v)
 			tmux set-option -p -t "${pane}" remain-on-exit on
+			tmux set-option -w remain-on-exit on
+			pane=$(tmux split-window -P -d -h 'ulimit -n 4096 && godoc -http=:6060 -notes="BUG|TODO"')
+			tmux set-option -w remain-on-exit off
 			tmux select-pane -t 0
 			tmux select-layout main-vertical
 			if [ -d "$GOPATH/src/github.com" ]
